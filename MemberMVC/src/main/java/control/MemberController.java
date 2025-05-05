54f8abedd3a2ee12aa717f424084e8d7abd3a55c
Zayne <star1582@naver.com>
2025-05-05 18:44:08 +0900

    회원가입 기능 개발

---
+++ MemberMVC/src/main/java/control/MemberController.java
@@ -0,0 +1,60 @@
+package control;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.MemberBean;
+import model.MemberDAO;
+
+/**
+ * Servlet implementation class MemberController
+ */
+@WebServlet("/Proc.do")
+public class MemberController extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+	
+	protected void doProc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		MemberBean bean = new MemberBean();
+		bean.setId(request.getParameter("id"));
+		bean.setPass1(request.getParameter("pass1"));
+		bean.setEmail(request.getParameter("email"));
+		bean.setTel(request.getParameter("tel"));
+		
+		String[] hobbys = request.getParameterValues("hobby");
+		String hobby = "";
+		for (String string : hobbys) {
+			hobby += string + " ";
+		}
+		
+		bean.setHobby(hobby);
+		bean.setJob(request.getParameter("job"));
+		bean.setAge(request.getParameter("age"));
+		bean.setInfo(request.getParameter("info"));
+		
+		MemberDAO dao = new MemberDAO();
+		dao.insertMember(bean);
+		
+		RequestDispatcher dis = request.getRequestDispatcher("MemberList.jsp");
+		dis.forward(request, response);
+		
+		
+		
+	}
+
+}
---
+++ MemberMVC/src/main/java/model/MemberDAO.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	Connection con;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	
+	public void insertMember(MemberBean bean) {
+		try {
+			getCon();
+			String sql = "insert into member values (?,?,?,?,?,?,?,?)";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, bean.getId());
+			pstmt.setString(2, bean.getPass1());
+			pstmt.setString(3, bean.getEmail());
+			pstmt.setString(4, bean.getTel());
+			pstmt.setString(5, bean.getHobby());
+			pstmt.setString(6, bean.getJob());
+			pstmt.setString(7, bean.getAge());
+			pstmt.setString(8, bean.getInfo());
+			
+			pstmt.executeUpdate();
+			
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void getCon() throws SQLException {
+		try {
+			Context initCtx = new InitialContext();
+			DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/pool");
+			this.con = ds.getConnection();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
